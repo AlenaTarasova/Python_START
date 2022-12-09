ad87bd5edb76492f67293b16bf7f3313f455bfb1
Alena <kyklyoshka.kisyy@mail.ru>
2022-12-09 14:23:25 +0300

    2

--- HW_2/task_1.py
+++ HW_2/task_1.py
@@ -11,3 +11,15 @@
 0.56
 11
 """
+num = input("Введите дробное число (целую часть отделить точкой): ")  
+x = num.split(".") 
+a = int(x[0]) 
+b = int(x[1]) 
+sum = 0
+while (a > 0): 
+    sum = sum + (a % 10)
+    a = a // 10
+while (b > 0): 
+    sum = sum + (b % 10)
+    b = b // 10
+print(sum)
