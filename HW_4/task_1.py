1f238c87e4eecbc91a01da07a2312bf3c1148e21
Alena <kyklyoshka.kisyy@mail.ru>
2022-12-16 14:48:13 +0300

    1

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,15 @@
 38
 [2, 19]
 """
+n = int(input("Введите n : "))
+factors = []
+d = 2
+m = n
+while d * d <= n:
+    if n % d == 0:
+        factors.append(d)
+        n //= d
+    else:
+        d += 1
+factors.append(n)
+print('{} = {}' .format(m, factors))
